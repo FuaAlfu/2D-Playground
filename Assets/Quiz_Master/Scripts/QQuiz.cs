ff6d50519dc0264b2bb3dff3f45018f46e29a9a1
FuaAlfu <55155669+FuaAlfu@users.noreply.github.com>
2022-03-09 22:05:46 +0300

    add

--- Assets/Quiz_Master/Scripts/QQuiz.cs
+++ Assets/Quiz_Master/Scripts/QQuiz.cs
@@ -15,10 +15,24 @@ public class QQuiz : MonoBehaviour
     [SerializeField]
     TextMeshProUGUI text;
 
+    [SerializeField]
+    GameObject[] buttons;
+
     // Start is called before the first frame update
     void Start()
     {
         text.text = qQuestions.GetQuestion();
+
+        for(int i = 0; i < buttons.Length; i++)
+        {
+            TextMeshProUGUI buttonTxt = buttons[i].GetComponentInChildren<TextMeshProUGUI>();
+            buttonTxt.text = qQuestions.GetAnswer(i);
+        }
+
+        /* for testing
+            TextMeshProUGUI buttonTxt = buttons[0].GetComponentInChildren<TextMeshProUGUI>();
+            buttonTxt.text = qQuestions.GetAnswer(0);
+         */
     }
 
     // Update is called once per frame
